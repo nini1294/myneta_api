4a7d401a94951d69ee96f1c442d74d276286aba5
Nishant <nishant301294@gmail.com>
2015-07-22 23:21:00 +0530

    Fixes a require error

--- models.rb
+++ models.rb
@@ -1,2 +1,9 @@
-require 'pg'
+require 'sequel'
+$: << '../lib'
 
+Sequel::Model.plugin :prepared_statements
+Sequel::Model.plugin :validation_helpers
+
+DB = Sequel.connect(ENV['DATABASE_URL'])
+
+%w'mla'.each{|model| require "./models/#{model}.rb"}
--- models/mlas.rb
+++
@@ -1,3 +0,0 @@
-class MLA < Sequel::Model
-    
-end
