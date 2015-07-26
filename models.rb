4e9f0f505267b3ea58ab5aaec208b426fc16d891
nini1294 <nishant301294@gmail.com>
2015-07-26 20:20:34 +0530

    Adds a new table for MPs

--- models.rb
+++ models.rb
@@ -7,4 +7,4 @@ Sequel::Model.plugin :validation_helpers
 ENV['DATABASE_URL'] = 'postgres://pmyvxrkrgxnvxz:-sB3vkxwYpPYxLdBvLQyhMsVJc@ec2-54-83-20-177.compute-1.amazonaws.com:5432/d3volvoen2akce' unless ENV.include?('DATABASE_URL')
 DB = Sequel.connect(ENV['DATABASE_URL'])
 
-%w'mla'.each{|model| require "./models/#{model}.rb"}
+%w'mla mp'.each{|model| require "./models/#{model}.rb"}
