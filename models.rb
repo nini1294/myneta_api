279531770d2698a6d3b7587c4bc48b422ece12c0
nini1294 <nishant301294@gmail.com>
2015-07-27 20:47:23 +0530

    Replaces the ENV hash lookup with a fetch call

--- models.rb
+++ models.rb
@@ -4,7 +4,7 @@ $: << '../lib'
 Sequel::Model.plugin :prepared_statements
 Sequel::Model.plugin :validation_helpers
 
-ENV['DATABASE_URL'] = 'postgres://pmyvxrkrgxnvxz:-sB3vkxwYpPYxLdBvLQyhMsVJc@ec2-54-83-20-177.compute-1.amazonaws.com:5432/d3volvoen2akce' unless ENV.include?('DATABASE_URL')
-DB = Sequel.connect(ENV['DATABASE_URL'])
+DBURL = ENV.fetch('DATABASE_URL') {'postgres://pmyvxrkrgxnvxz:-sB3vkxwYpPYxLdBvLQyhMsVJc@ec2-54-83-20-177.compute-1.amazonaws.com:5432/d3volvoen2akce'}
+DB = Sequel.connect(DBURL)
 
 %w'mla mp'.each{|model| require "./models/#{model}.rb"}
