f8e0c4a000f9d380825e2fb750c776941429ae87
nini1294 <nishant301294@gmail.com>
2015-08-02 23:47:59 +0530

    Adds a format helper method to the MP model

--- models/mp.rb
+++ models/mp.rb
@@ -1,3 +1,12 @@
 class MP < Sequel::Model
-
+    # Helper formatting method, takes a parameter specifying what to delete
+    def format(delete = [:mp_id])
+        tmp = @values
+        # Delete each parameter passed in
+        delete.each do |param|
+            tmp.delete(param)
+        end
+        tmp[:assets] = tmp[:assets].to_f unless tmp[:assets].nil?
+        return tmp
+    end
 end
