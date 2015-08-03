deced3ff77559cd82f8f3b74d887c092a904542c
nini1294 <nishant301294@gmail.com>
2015-08-03 11:32:22 +0530

    Changes to prepare for release

--- models.rb
+++ models.rb
@@ -4,7 +4,7 @@ $: << '../lib'
 Sequel::Model.plugin :prepared_statements
 Sequel::Model.plugin :validation_helpers
 
-DBURL = ENV.fetch('DATABASE_URL') {'postgres://pmyvxrkrgxnvxz:-sB3vkxwYpPYxLdBvLQyhMsVJc@ec2-54-83-20-177.compute-1.amazonaws.com:5432/d3volvoen2akce'}
+DBURL = ENV.fetch('DATABASE_URL')
 DB = Sequel.connect(DBURL)
 
 %w'mla mp'.each{|model| require "./models/#{model}.rb"}
