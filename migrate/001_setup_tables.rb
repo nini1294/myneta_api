c1dd2d21c8ba402ea9609131bff7c30e1615d5c9
Nishant <nishant301294@gmail.com>
2016-04-04 22:12:33 -0400

    Updated ash syntax

--- migrate/001_setup_tables.rb
+++ migrate/001_setup_tables.rb
@@ -4,13 +4,13 @@ Sequel.migration do
     up do
         create_table(:mlas) do
             primary_key :mla_id
-            String :state, :null => false
-            String :name, :null => false
-            String :constituency, :null => false
-            String :party, :null => false
-            Integer :criminal_cases, :null => false
-            String :education, :null => false
-            BigDecimal :assets, :size=>[12, 2]
+            String :state, null: false
+            String :name, null: false
+            String :constituency, null: false
+            String :party, null: false
+            Integer :criminal_cases, null: false
+            String :education, null: false
+            BigDecimal :assets, size: [12, 2]
         end
     end
 
--- migrate/003_add_mp_table.rb
+++ migrate/003_add_mp_table.rb
@@ -2,14 +2,14 @@ Sequel.migration do
     up do
         create_table(:mps) do
             primary_key :mp_id
-            String :state, :null => false
-            String :name, :null => false
-            String :constituency, :null => false
-            String :party, :null => false
-            Integer :criminal_cases, :null => false
-            String :education, :null => false
-            BigDecimal :assets, :size=>[12, 2]
-            Integer :year, :null => false
+            String :state, null: false
+            String :name, null: false
+            String :constituency, null: false
+            String :party, null: false
+            Integer :criminal_cases, null: false
+            String :education, null: false
+            BigDecimal :assets, size: [12, 2]
+            Integer :year, null: false
             unique([:name, :constituency, :state, :year], :name => :unique_mp)
         end
     end
